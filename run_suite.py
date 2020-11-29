f9b5db40fa148c101a321b88f1e39f8edc8508b9
a953732125 <953732125@qq.com>
2020-11-29 15:26:12 +0800

    sss

--- run_suite.py
+++ run_suite.py
@@ -8,3 +8,4 @@ suite = unittest.defaultTestLoader.discover('./scripts', pattern='test0*.py')
 report_path = app.DIR_NAME + os.sep + 'report' + os.sep + 'report{}.html'.format(int(time.time() * 1000))
 with open(report_path, 'wb')as f:
     HTMLTestRunner(stream=f).run(suite)
+    
